a7c3a1d0cdc636f3849f84e5fda92015f87b4263
InigoDiazMunio <i.diazmunio@opendeusto.es>
2024-11-12 19:01:06 +0100

    plantilla basica html

--- appRecetario/urls.py
+++ appRecetario/urls.py
@@ -1,11 +1,10 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('recetas/', views.list_recetas, name='list_recetas'),
-    path('recetas/<int:id>/', views.detail_receta, name='detail_receta'),
+    path('recetas/<int:receta_id>/', views.detail_receta, name='detail_receta'),
     path('ingredientes/', views.list_ingredientes, name='list_ingredientes'),
     path('ingredientes/<int:id>/', views.detail_ingrediente, name='detail_ingrediente'),
     path('tipos_plato/', views.list_tipos_plato, name='list_tipos_plato'),
--- appRecetario/views.py
+++ appRecetario/views.py
@@ -1,30 +1,40 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Receta,Ingrediente,TipoPlato
+from .models import Receta, Ingrediente, TipoPlato
 
+# Vista para la página principal
 def index(request):
     return render(request, 'index.html')
 
+# Vista para listar todas las recetas
 def list_recetas(request):
     recetas = Receta.objects.all()
     return render(request, 'recetas/list.html', {'recetas': recetas})
 
-def detail_receta(request, id):
-    receta = Receta.objects.get(id=id)
-    return render(request, 'recetas/detail.html', {'receta': receta})
-
 def detail_receta(request, receta_id):
     receta = get_object_or_404(Receta, id=receta_id)
     ingredientes = receta.ingredientes.all()
-    return render(request, 'detalles_receta.html', {'receta': receta, 'ingredientes': ingredientes})
+    return render(request, 'recetas/detail.html', {'receta': receta, 'ingredientes': ingredientes})
 
+# Vista para ver los detalles de un ingrediente específico
 def detail_ingrediente(request, id):
-    ingrediente = Ingrediente.objects.get(id=id)
+    ingrediente = get_object_or_404(Ingrediente, id=id)
     return render(request, 'ingredientes/detail.html', {'ingrediente': ingrediente})
 
+# Vista para listar todos los tipos de plato
 def list_tipos_plato(request):
     tipos_plato = TipoPlato.objects.all()
     return render(request, 'tipos_plato/list.html', {'tipos_plato': tipos_plato})
 
+# Vista para ver los detalles de un tipo de plato específico
 def detail_tipo_plato(request, id):
-    tipo_plato = TipoPlato.objects.get(id=id)
+    tipo_plato = get_object_or_404(TipoPlato, id=id)
     return render(request, 'tipos_plato/detail.html', {'tipo_plato': tipo_plato})
+def index(request):
+    tipos_plato = TipoPlato.objects.all()
+    recetas = Receta.objects.all()
+    ingredientes = Ingrediente.objects.all()
+    return render(request, 'index.html', {
+        'tipos_plato': tipos_plato,
+        'recetas': recetas,
+        'ingredientes': ingredientes
+    })
