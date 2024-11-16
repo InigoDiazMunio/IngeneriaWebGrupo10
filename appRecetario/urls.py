0954fd0ecba7063cb9dadc4f19973aa22877774c
InigoDiazMunio <i.diazmunio@opendeusto.es>
2024-11-16 11:28:22 +0100

    Pagina funcionando

--- appRecetario/urls.py
+++ appRecetario/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
-from . import views
+from appRecetario import views
 
 urlpatterns = [
+    path('', views.list_recetas, name='index'),
     path('recetas/', views.list_recetas, name='list_recetas'),
     path('recetas/<int:receta_id>/', views.detail_receta, name='detail_receta'),
     path('ingredientes/', views.list_ingredientes, name='list_ingredientes'),
