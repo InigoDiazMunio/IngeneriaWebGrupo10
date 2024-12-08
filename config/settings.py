8f1b4565d89587bef4ecbea93e8ad9dc1da88d20
nachomanero <ignacio.manero@opendeusto.es>
2024-12-08 11:26:22 +0100

    Mejoras de seguridad

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,20 @@
 from pathlib import Path
 import os
+from decouple import config, Csv
 from django.utils.translation import gettext_lazy as _
 
+# Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-m02i-e*tbfob65)#@jbueg#&b6^$%y)twzd%qa=bv+0id^36o('
+# SECRET_KEY desde .env
+SECRET_KEY = config('SECRET_KEY', default='tu-clave-segura')
 
-DEBUG = True
+# Entorno (dev o prod)
+ENVIRONMENT = config('ENVIRONMENT', default='dev')  # dev por defecto
 
-ALLOWED_HOSTS = []
+# Configuración base común
+DEBUG = ENVIRONMENT == 'dev'
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
 INSTALLED_APPS = [
     'appRecetario',
@@ -24,7 +30,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',  # Middleware para gestionar idiomas
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -59,36 +65,32 @@ DATABASES = {
     }
 }
 
-# Configuración de idiomas y localización
-LANGUAGE_CODE = 'es'  # Idioma predeterminado (Español)
-
+# Configuración de idiomas
+LANGUAGE_CODE = 'es'
 LANGUAGES = [
     ('en', _('English')),
     ('es', _('Español')),
 ]
-
 USE_I18N = True
 USE_L10N = True
-
 LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
-
-
 TIME_ZONE = 'UTC'
-
 USE_TZ = True
 
+# Archivos estáticos y multimedia
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'appRecetario', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'appRecetario', 'static', 'media')
 
+# Configuración de login
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Collectstatic
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
@@ -98,10 +100,34 @@ STATICFILES_FINDERS = [
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 
-
+# Caché
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
         'LOCATION': 'unique-snowflake',
     }
 }
+
+# Seguridad: configuración específica por entorno
+if ENVIRONMENT == 'prod':
+    SECURE_HSTS_SECONDS = 31536000  # 1 año
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_SSL_REDIRECT = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = 'DENY'
+else:  # Configuración para desarrollo
+    SECURE_SSL_REDIRECT = False
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
+    SECURE_HSTS_SECONDS = 0
+    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+
+
+
+
+print(f"ENVIRONMENT: {ENVIRONMENT}")
